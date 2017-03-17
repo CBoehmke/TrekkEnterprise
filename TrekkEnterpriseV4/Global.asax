a485b1d49e256b25dc163ff88f9cf1bfe31dffcf
cboehmke <cboehmke@DEV-HP-Z420>
2017-03-17 14:30:16 -0500

    Add project files.

---
+++ TrekkEnterpriseV4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrekkEnterpriseV4.MvcApplication" Language="C#" %>
---
+++ TrekkEnterpriseV4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrekkEnterpriseV4.Startup))]
+namespace TrekkEnterpriseV4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
